1b285998175d9d85fe963397ac25b6e7e372a6e5
aorlando7793 <aorlando7793@gmail.com>
2017-11-09 13:45:51 -0500

    Add files via upload

--- thermal_lensing.py
+++ thermal_lensing.py
@@ -5,19 +5,19 @@ from cavityclass import *
 
 LAM = 1064*10**(-7)
 
-q_exp = -4.55 + 1j*27.8
+zr_inv = 
 
 ds28 = Cavity('datatest.dat',LAM)
 
 #laser_list = [newcav = ds28.remove_optic(5).insert_optic('L',i,5) for i in np.arange(15,25,.1) if newcav.is_stable]
 laser_list = []
 #loops through a range of thermal lensing values, creates a cavity for each one and appends it to laser_list.
-for i in np.arange(15,25,.01):
+for i in np.arange(10,40,.1):
     newcav = ds28.remove_optic(5).insert_optic('L',i,5)
     if newcav.is_stable:
         laser_list.append(newcav)
 #diff is a list of how much each cavity's initial q differs from the experimental value.
-diff = [abs(q_exp - laser.q0) for laser in laser_list]
+diff = [abs(zr_inv - (1/laser.q0.imag)) for laser in laser_list]
 #finds index of minimum difference
 min_i = diff.index(min(diff))
 best_lens = laser_list[min_i]
